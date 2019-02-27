80499c243b751498e28d66dd083dfca3899e8ba2
wyousef <wyousef@fcih.net>
2019-02-27 23:01:14 +0200

    Update

--- ProbStatI/Code/Matlab/ChSimulation/CodeRV.m
+++
@@ -1,3 +0,0 @@
-n=100; mu=0; sigma=1;
-x = random('normal', mu, sigma, [n,1])
-plot(x, zeros([length(x),1]), '.r');
--- ProbStatI/Code/Matlab/ChSimulation/MonteCarloSimulation.m
+++
@@ -1,7 +0,0 @@
-P={x1,...,xM}; %Data Generated
-S=0;
-for m=1:M
-    if (R(xm))
-        S=S+g(xm);
-end;
-ret=S/M;
